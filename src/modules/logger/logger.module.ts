6c625e57ebc743089128c904617efaefd8ec28e1
Serhiy-Krasovskyy <s.krasovskyy@iobroker.io>
2026-04-15 10:45:27 +0300

    Upgraded eslint.

--- src/modules/logger/logger.module.ts
+++ src/modules/logger/logger.module.ts
@@ -18,7 +18,7 @@ export const WINSTON_LOGGER = 'WINSTON_LOGGER';
                             winston.format.colorize(),
                             winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
                             winston.format.printf(({ timestamp, level, message, ...meta }) => {
-                                return `${String(timestamp)} [${level}]: ${String(message)} ${Object.keys(meta).length ? JSON.stringify(meta) : ''}`;
+                                return `${timestamp as string} [${level}]: ${message as string} ${Object.keys(meta).length ? JSON.stringify(meta) : ''}`;
                             }),
                         ),
                         level: config.logLevel || 'info',
@@ -32,7 +32,7 @@ export const WINSTON_LOGGER = 'WINSTON_LOGGER';
                         format: winston.format.combine(
                             winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
                             winston.format.printf(({ timestamp, level, message, ...meta }) => {
-                                return `${String(timestamp)} [${level}]: ${String(message)} ${Object.keys(meta).length ? JSON.stringify(meta) : ''}`;
+                                return `${timestamp as string} [${level}]: ${message as string} ${Object.keys(meta).length ? JSON.stringify(meta) : ''}`;
                             }),
                         ),
                     }),
